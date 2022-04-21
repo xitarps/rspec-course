9468c025b4b435855f68af7df4e15ad358d583c7
Rodrigo - Xita rps <xitarps@gmail.com>
2022-04-21 20:38:10 -0300

    [Refactor]: change before method for a 'card' method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,20 +1,19 @@
 require_relative '../card'
 
 RSpec.describe Card do
-  before do
-# before(:example) do
-    @card = Card.new(rank: 'Ace', suit: 'Spades')
+  def card
+    Card.new(rank: 'Ace', suit: 'Spades')
   end
 
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 
   it 'has a type' do
-    expect(@card.type).to eq('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades')
   end
 end
