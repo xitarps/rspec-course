6de204423108d2dc0f62079020dbc645ff223a72
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-04 03:34:28 -0300

    [Feature]: Add 'overwriting let variables'

---
+++ programming_language.rb
@@ -0,0 +1,7 @@
+class ProgrammingLanguage
+  attr_reader :name
+
+  def initialize(name = 'Ruby')
+    @name = name
+  end
+end
---
+++ spec/overwriting_let_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../programming_language'
+
+RSpec.describe ProgrammingLanguage do
+  let(:language) { ProgrammingLanguage.new('Python') }
+
+  it 'should store the name of the language' do
+    expect(language.name).to eq('Python')
+  end
+
+  context 'with no argument' do
+    let(:language) { ProgrammingLanguage.new }
+
+    it 'should default to Ruby as the name' do
+      expect(language.name).to eq('Ruby')
+    end
+  end
+end
