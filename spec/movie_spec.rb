1f1b18b2b9129403152f0525ad13bbbde7ab07a4
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-31 09:36:19 -0300

    [Wip]: Add movie spec tests

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,6 +1,21 @@
 require_relative '../movie'
 require_relative '../actor'
 
-# RSpec.describe '' do
-  
-# end
+RSpec.describe Movie do
+  let(:stuntman) { double('Mr. Danger', ready?: true,
+                                        act: 'Any string at all',
+                                        fall_off_ladder: 'Sure, lets do it',
+                                        light_on_fire: true) }
+
+  subject { described_class.new(stuntman) }
+
+  describe "#start_shooting method" do
+    it 'expects an actor to do 3 actions' do
+      expect(stuntman).to receive(:ready?)
+      expect(stuntman).to receive(:act)
+      expect(stuntman).to receive(:fall_off_ladder)
+      expect(stuntman).to receive(:light_on_fire)
+      subject.start_shooting
+    end
+  end
+end
